43b1b363c981242c9fb961ee517725bd793b0e2f
Rusiru Dilshan <12104625+rusirudilsh@users.noreply.github.com>
2024-09-27 22:35:20 +0100

    routes fixes

--- product-store-front/src/services/ProductService.ts
+++ product-store-front/src/services/ProductService.ts
@@ -7,11 +7,11 @@ const headers = EnvironmentSettings.productsAPISettings.headers;
 
 export const productList = async (category: string, isByAvailability: boolean, first: number, per_page_count: number) => 
     await axios.get(EnvironmentSettings.productsAPISettings.baseUrl + 
-        `product/list?category=${category}&stock_availability=${isByAvailability}&first=${first}&products_per_page=${per_page_count}`,
+        `products/list?category=${category}&stock_availability=${isByAvailability}&first=${first}&products_per_page=${per_page_count}`,
          {headers});
 
 export const categoryList = async () => 
-    await axios.get(EnvironmentSettings.productsAPISettings.baseUrl + 'category/list', {headers});
+    await axios.get(EnvironmentSettings.productsAPISettings.baseUrl + 'categories/list', {headers});
 
 export const postOrder = async (order: ProductOrder) => 
-    await axios.post(EnvironmentSettings.productsAPISettings.baseUrl + 'order/', JSON.stringify(order), {headers});
+    await axios.post(EnvironmentSettings.productsAPISettings.baseUrl + 'orders/', JSON.stringify(order), {headers});
